d789f9ac46c28e461f355ed1f709e84f9146e82a
sridharshankar25 <sridhar25600@gmail.com>
2020-08-09 20:53:50 +0530

    main page and traditional page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,16 +22,4 @@ export class HeaderComponent implements OnInit {
    }
   }
 
-
-  myAccFunc() {
-    var x = document.getElementById("demoAcc");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-      x.previousElementSibling.className += " w3-grey";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-      x.previousElementSibling.className = 
-      x.previousElementSibling.className.replace(" w3-grey", "");
-    }
-  }
 }
