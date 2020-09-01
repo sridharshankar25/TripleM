fb3e470d2b673b1a0aff59456d9eab66f57fc720
sridharshankar25 <sridhar25600@gmail.com>
2020-09-01 22:01:46 +0530

    About page update3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,9 @@ export class AppComponent {
     // config.easing = 'cubic-bezier(0.645, 0.045, 0.355, 1)';
 
     //other options here
+
+    onActivate(event)
+    {
+      window.scroll(0,0);
+    }
   }
